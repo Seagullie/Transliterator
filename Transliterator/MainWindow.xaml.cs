02244be7a1037b2f1dd5a8bc02bae9aaab84f267
Seagullie <59409233+Seagullie@users.noreply.github.com>
2023-02-10 05:32:20 +0200

    Transliterator: Finish migrating main window UI to new framework

--- Transliterator/MainWindow.xaml.cs
+++ Transliterator/MainWindow.xaml.cs
@@ -12,17 +12,38 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Transliterator.ViewModels;
+using Wpf.Ui.Mvvm.Interfaces;
 
 namespace Transliterator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        public MainWindowViewModel ViewModel { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            ViewModel = new();
+            DataContext = ViewModel;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Make window draggable
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // TODO: Uncomment after migrating other things referenced by the methods
+
+            //ViewModel.SaveSettings();
+            //ViewModel.DisposeOfNotifyIcon();
         }
     }
 }
---
+++ Transliterator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Windows;
+using System.Windows.Forms;
+using Wpf.Ui.Common;
+using Wpf.Ui.Controls;
+
+namespace Transliterator.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        // TODO: Rewrite to bool
+        [ObservableProperty]
+        private string _appState;
+
+        [ObservableProperty]
+        private string _toggleAppStateShortcut;
+
+        [ObservableProperty]
+        private string _applicationTitle = String.Empty;
+
+        [ObservableProperty]
+        private ObservableCollection<object> _navigationItems = new();
+
+        [ObservableProperty]
+        private ObservableCollection<object> _navigationFooter = new();
+
+        [ObservableProperty]
+        private ObservableCollection<MenuItem> _trayMenuItems = new();
+
+        public MainWindowViewModel()
+        {
+            // TODO: Connect to backend
+            AppState = "On";
+            ToggleAppStateShortcut = "Alt + T";
+        }
+
+        private void InitializeViewModel()
+        {
+            ApplicationTitle = "Transliterator";
+
+            TrayMenuItems = new ObservableCollection<MenuItem>
+            {
+                new MenuItem
+                {
+                    Header = "Home",
+                    Tag = "tray_home"
+                }
+            };
+        }
+    }
+}

0723d0546df20fbfe0d9bb7c5de23c987c3cdee4
Seagullie <59409233+Seagullie@users.noreply.github.com>
2023-02-12 23:36:34 +0200

    Migrate ToggleSounds Window from old project

---
+++ Transliterator/ViewModels/EditToggleSoundsViewModel.cs
@@ -0,0 +1,93 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.IO;
+
+// TODO: Uncomment after migrating other things from old project
+
+// using Transliterator.Services;
+
+namespace Transliterator.ViewModels
+{
+    public partial class EditToggleSoundsViewModel : ObservableObject
+    {
+        // TODO: Uncomment after migrating other things from old project
+
+        // private readonly SettingsService settingsService;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(ToggleOnSoundFileName))]
+        private string toggleOnSoundFilePath;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(ToggleOffSoundFileName))]
+        private string toggleOffSoundFilePath;
+
+        public string ToggleOnSoundFileName { get => Path.GetFileName(ToggleOnSoundFilePath) ?? "<None>"; }
+
+        public string ToggleOffSoundFileName { get => Path.GetFileName(ToggleOffSoundFilePath) ?? "<None>"; }
+
+        public EditToggleSoundsViewModel()
+        {
+            // TODO: Uncomment after migrating other things from old project
+
+            //settingsService = SettingsService.GetInstance();
+            //ToggleOnSoundFilePath = settingsService.PathToCustomToggleOnSound;
+            //ToggleOffSoundFilePath = settingsService.PathToCustomToggleOffSound;
+        }
+
+        [RelayCommand]
+        private void ChangeToggleOnSound()
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Audio documents (.wav)|*.wav" // Filter files by extension
+            };
+
+            bool? result = dialog.ShowDialog();
+
+            if (result != null && result == true)
+            {
+                // Open document
+                string pathToFile = dialog.FileName;
+                ToggleOnSoundFilePath = pathToFile;
+                //settingsService.PathToCustomToggleOnSound = pathToFile;
+            }
+        }
+
+        // TODO: Uncomment after migrating other things from old project
+        [RelayCommand]
+        private void ChangeToggleOffSound()
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Audio documents (.wav)|*.wav" // Filter files by extension
+            };
+
+            bool? result = dialog.ShowDialog();
+
+            if (result != null && result == true)
+            {
+                // Open document
+                string pathToFile = dialog.FileName;
+                ToggleOffSoundFilePath = pathToFile;
+                //settingsService.PathToCustomToggleOffSound = pathToFile;
+            }
+        }
+
+        // TODO: Uncomment after migrating other things from old project
+
+        [RelayCommand]
+        private void DeleteToggleOnSound()
+        {
+            ToggleOnSoundFilePath = null;
+            //settingsService.PathToCustomToggleOnSound = null;
+        }
+
+        [RelayCommand]
+        private void DeleteToggleOffSound()
+        {
+            ToggleOffSoundFilePath = null;
+            //settingsService.PathToCustomToggleOffSound = null;
+        }
+    }
+}
---
+++ Transliterator/Views/EditToggleSoundsWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+//using Transliterator.Services;
+using Transliterator.ViewModels;
+
+namespace Transliterator.Views
+{
+    public partial class EditToggleSoundsWindow
+    {
+        public EditToggleSoundsViewModel ViewModel { get; private set; }
+
+        public EditToggleSoundsWindow()
+        {
+            InitializeComponent();
+            ViewModel = new();
+            DataContext = ViewModel;
+        }
+
+        // TODO: Remove this duct tape
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            // Remove unpainted white area around borders
+            void action() => InvalidateMeasure();
+            Dispatcher.BeginInvoke((Action)action);
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Make window draggable
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+    }
+}

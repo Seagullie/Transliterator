3dfc776ac21a1984219bfa074611947f8d8438e0
bind-w-exit <12Andriy24@Gmail.com>
2023-03-06 19:43:31 +0200

    Add Singleton wrapper class

---
+++ Transliterator.Core/Services/HotKeyService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transliterator.Core.Services
+{
+    public class HotKeyService
+    {
+    }
+}

18038ebe8188e60f289795a7d9c8c6385dc5c1a6
bind-w-exit <12Andriy24@Gmail.com>
2023-03-07 15:24:54 +0200

    Changed NativeMethods visibility

--- Transliterator.Core/Native/NativeMethods.User32.cs
+++ Transliterator.Core/Native/NativeMethods.User32.cs
@@ -8,24 +8,24 @@ internal static partial class NativeMethods
 {
     private const string User32 = "user32.dll";
 
-    public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+    internal delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
 
     [DllImport(User32, CharSet = CharSet.Auto, SetLastError = true)]
-    public static extern IntPtr SetWindowsHookEx(HookTypes hookType, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+    internal static extern IntPtr SetWindowsHookEx(HookTypes hookType, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
     [DllImport(User32, CharSet = CharSet.Auto, SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+    internal static extern bool UnhookWindowsHookEx(IntPtr hhk);
 
     [DllImport(User32, CharSet = CharSet.Auto, SetLastError = true)]
-    public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+    internal static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
 
     /// <summary>
     ///     Wrapper to simplify sending of inputs
     /// </summary>
     /// <param name="inputs">Input array</param>
     /// <returns>inputs send</returns>
-    public static uint SendInput(Structs.Input[] inputs)
+    internal static uint SendInput(Structs.Input[] inputs)
     {
         return SendInput((uint)inputs.Length, inputs, Structs.Input.Size);
     }
@@ -37,16 +37,16 @@ internal static partial class NativeMethods
     ///     To get extended error information, call GetLastError.
     /// </summary>
     [DllImport(User32, SetLastError = true)]
-    private static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray)][In] Structs.Input[] inputs, int cbSize);
+    internal static extern uint SendInput(uint nInputs, [MarshalAs(UnmanagedType.LPArray)][In] Structs.Input[] inputs, int cbSize);
 
     [DllImport(User32, CharSet = CharSet.Auto, ExactSpelling = true)]
-    public static extern IntPtr GetKeyboardLayout(uint dwLayout);
+    internal static extern IntPtr GetKeyboardLayout(uint dwLayout);
 
     [DllImport(User32)]
-    public static extern int ToUnicodeEx(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags, IntPtr dwhkl);
+    internal static extern int ToUnicodeEx(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr)] System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags, IntPtr dwhkl);
 
     [DllImport(User32)]
-    public static extern bool GetKeyboardState(byte[] lpKeyState);
+    internal static extern bool GetKeyboardState(byte[] lpKeyState);
 
     /// <summary>
     ///     Retrieve the state of a key
@@ -55,15 +55,14 @@ internal static partial class NativeMethods
     /// <returns></returns>
     [DllImport(User32, ExactSpelling = true)]
     [ResourceExposure(ResourceScope.None)]
-    public static extern ushort GetKeyState(VirtualKeyCode keyCode);
+    internal static extern ushort GetKeyState(VirtualKeyCode keyCode);
 
     [DllImport(User32)]
-    public static extern uint MapVirtualKey(uint uCode, uint uMapType);
+    internal static extern uint MapVirtualKey(uint uCode, uint uMapType);
 
     [DllImport(User32)]
-    public static extern IntPtr GetForegroundWindow();
+    internal static extern IntPtr GetForegroundWindow();
 
     [DllImport(User32)]
-    public static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr lpdwProcessId);
-
+    internal static extern uint GetWindowThreadProcessId(IntPtr hWnd, IntPtr lpdwProcessId);
 }
